bf45deac10656a127a96f4c9df240b8ee2b0d920
dsb67 <diannasb67@gmail.com>
2019-03-31 22:15:52 -0700

    updatedApp

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -5,11 +5,11 @@ import FlowerCard from "./components/FlowerCard/image";
 import flowers from "./flowers.json";
 
 
-
 class App extends React.Component {
   state = {
     count: 0
   };
+
 
   handleIncrement = () => {
     this.setState({ count: this.state.count + 1 });
@@ -21,60 +21,60 @@ class App extends React.Component {
 
     return (
       <div>
-              <nav className="navbar navbar-dark bg-dark navnav">
-                <span className="navbar-brand navFont">
-                    <h1>Clicky Game</h1>
-                </span>
-                <span className="navbar-brand navFont">
-                    <h1>Click and Image to Begin</h1>
-                </span>
-                <span className="navbar-brand navFont">
-                    <h1>Score: {this.state.count} | Top Score:</h1>
-                </span>
-            </nav>
-        {/* <Navbar /> */}
+        <nav className="navbar navbar-dark bg-dark navnav">
+          <span className="navbar-brand navFont">
+            <h1>Clicky Game</h1>
+          </span>
+          <span className="navbar-brand navFont">
+            <h1>Click and Image to Begin</h1>
+          </span>
+          <span className="navbar-brand navFont">
+            <h1>Score: {this.state.count} | Top Score:</h1>
+          </span>
+        </nav>
+
         <FlowerCard>
-          
-            <FlowerCard
-              image={flowers[0].image}
-              click={this.handleIncrement}
-            />
-              <FlowerCard
-              image={flowers[1].image}
-              click={this.handleIncrement}
-            />
-              <FlowerCard
-              image={flowers[2].image}
-              click={this.handleIncrement}
-            />
-              <FlowerCard
-              image={flowers[3].image}
-              click={this.handleIncrement}
-            />
-              <FlowerCard
-              image={flowers[4].image}
-              click={this.handleIncrement}
-            />
-              <FlowerCard
-              image={flowers[5].image}
-              click={this.handleIncrement}
-            />
-              <FlowerCard
-              image={flowers[6].image}
-              click={this.handleIncrement}
-            />
-              <FlowerCard
-              image={flowers[7].image}
-              click={this.handleIncrement}
-            />
-              <FlowerCard
-              image={flowers[8].image}
-              click={this.handleIncrement}
-            />
-              <FlowerCard
-              image={flowers[9].image}
-              click={this.handleIncrement}
-            />
+
+          <FlowerCard
+            image={flowers[0].image}
+            click={this.handleIncrement}
+          />
+          <FlowerCard
+            image={flowers[1].image}
+            click={this.handleIncrement}
+          />
+          <FlowerCard
+            image={flowers[2].image}
+            click={this.handleIncrement}
+          />
+          <FlowerCard
+            image={flowers[3].image}
+            click={this.handleIncrement}
+          />
+          <FlowerCard
+            image={flowers[4].image}
+            click={this.handleIncrement}
+          />
+          <FlowerCard
+            image={flowers[5].image}
+            click={this.handleIncrement}
+          />
+          <FlowerCard
+            image={flowers[6].image}
+            click={this.handleIncrement}
+          />
+          <FlowerCard
+            image={flowers[7].image}
+            click={this.handleIncrement}
+          />
+          <FlowerCard
+            image={flowers[8].image}
+            click={this.handleIncrement}
+          />
+          <FlowerCard
+            image={flowers[9].image}
+            click={this.handleIncrement}
+          />
 
         </FlowerCard>
 
